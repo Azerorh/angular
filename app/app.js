c12e09ef619b341809ea2c6c5bff6335197edd3c
Shirokonos Dmitry <azerorh@gmail.com>
2019-09-01 17:15:55 +0300

    angular heroku fixes

--- app/app.js
+++ app/app.js
@@ -12,14 +12,3 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 
   $routeProvider.otherwise({redirectTo: '/todo'});
 }]);
-
-var express = require('express');
-var path = require('path');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
-app.listen(process.env.PORT || 3000);
